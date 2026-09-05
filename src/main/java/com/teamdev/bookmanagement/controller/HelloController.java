0484ff950f78f75e9313ac20c6a8ff7b7ebb5962
yinyinning <zhuyinning7892@qq.com>
2026-09-05 18:29:00 +0800

    完成验证用的HelloController

---
+++ src/main/java/com/teamdev/bookmanagement/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.teamdev.bookmanagement.controller;
+
+import com.teamdev.bookmanagement.common.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public Result<String> hello(){
+        return Result.success("hello,world");
+    }
+}
