31bd91010979161b96376276a47ec520409cb6f3
yinyinning <zhuyinning7892@qq.com>
2026-09-05 16:48:14 +0800

    搭建Spring Boot骨架

---
+++ src/main/java/com/teamdev/bookmanagement/BookManagementApplication.java
@@ -0,0 +1,11 @@
+package com.teamdev.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementApplication.class,args);
+    }
+}
